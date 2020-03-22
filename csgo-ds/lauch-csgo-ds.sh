c26ff5862aaffe3a5e6d94ad4e863a80a9b56539
Leonel Reyes <leonel.reyes@xoom.com>
2020-03-22 13:07:21 -0600

    only commit (I hope)

---
+++ csgo-ds/lauch-csgo-ds.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# default value 
+#~/csgo-ds/srcds_run -game csgo -net_port_try 1 +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount XXXXXXXXXXXXXX
+#
+# Arms Race 
+~/csgo-ds/srcds_run -game csgo -net_port_try 1 +game_type 1 +game_mode 0 +mapgroup mg_armsrace +sv_setsteamaccount XXXXXXXXXXXXXX
