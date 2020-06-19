a92464590c8a73734fe63ee26f6f4c4de35e8e30
SiRGt <lreyes6@gmail.com>
2020-06-19 09:30:56 -0600

    update on launch script
    Adding game modes and update

--- csgo-ds/lauch-csgo-ds.sh
+++ csgo-ds/lauch-csgo-ds.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
-# default value 
-#~/csgo-ds/srcds_run -game csgo -net_port_try 1 +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount XXXXXXXXXXXXXX
-#
-# Arms Race 
-~/csgo-ds/srcds_run -game csgo -net_port_try 1 +game_type 1 +game_mode 0 +mapgroup mg_armsrace +sv_setsteamaccount XXXXXXXXXXXXXX
+# XXXXXXXXXX = your_steam_id
+
+if [ -z $1 ]; then
+	echo "Need Game type (casual, competitive, arms, demo, update)"
+	exit 1
+fi
+
+case $1 in
+	casual)
+		echo "Casual"
+		~/csgo-ds/srcds_run -game csgo -net_port_try 1 +game_type 0 +game_mode 0 +sv_setsteamaccount XXXXXXXXXX
+		;;
+	competitive)
+		echo "Competitive"
+		~/csgo-ds/srcds_run -game csgo -net_port_try 1 +game_type 1 +game_mode 1 +mapgroup mg_armsrace +sv_setsteamaccount XXXXXXXXXX
+		;;
+	arms)
+		echo "Arms Race"
+		~/csgo-ds/srcds_run -game csgo -net_port_try 1 +game_type 1 +game_mode 0 +mapgroup mg_armsrace +sv_setsteamaccount XXXXXXXXXX
+		;;
+	demo)
+		echo "Demolition"
+		~/csgo-ds/srcds_run -game csgo -net_port_try 1 +game_type 1 +game_mode 1 +mapgroup mg_demolition +sv_setsteamaccount XXXXXXXXXX
+		;;
+	update)
+		echo "updating"
+		steamcmd +force_install_dir ~/csgo-ds/ +login anonymous +app_update 740 +quit
+		;;
+	*)
+		echo "Don't know what to do"
+		exit 1
+		;;
+esac
